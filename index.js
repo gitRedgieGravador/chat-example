8a98055c637e3235e6683ab3ebf242c393ffc3e4
cjcabungcagcls <2ndyrGroupC@pn.local>
2019-09-11 16:33:41 +0800

    initial commit 9-11-19

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
-
+var users = [];
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
@@ -10,6 +10,13 @@ app.get('/', function(req, res){
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
+  });
+
+  socket.on('online', function(msg){
+    if(!users.includes(msg)){
+      users.push(msg);
+    }
+    io.emit('online', users);
   });
 });
 

b1ec46ec9c71bf132dc3604538e867151cdd4c41
gitRedgieGravador <iderge32@gmail.com>
2019-09-12 09:31:23 +0800

    Add avatar

--- index.js
+++ index.js
@@ -1,25 +1,34 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var fs = require('fs');
+
+
 var port = process.env.PORT || 3000;
 var users = [];
-app.get('/', function(req, res){
+
+app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
+io.on('connection', function (socket) {
+  socket.on('chat message', function (msg) {
+    users.forEach(element => {
+      if(element.username == msg.username){
+        msg = {username: msg.username, msg: msg.msg, source: element.source}
+      }
+    });
     io.emit('chat message', msg);
   });
 
-  socket.on('online', function(msg){
-    if(!users.includes(msg)){
+  socket.on('online', function (msg) {
+    if (!users.includes(msg.username)) {
       users.push(msg);
     }
     io.emit('online', users);
   });
 });
 
-http.listen(port, function(){
+http.listen(port, function () {
   console.log('listening on *:' + port);
 });
